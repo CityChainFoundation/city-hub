956b38ef3957ba7fa207b159f381bcf265665b10
Sondre Bjellås <pixelworld@hotmail.com>
2018-08-30 01:25:03 +0200

    Add support for entering extension word
    - Add extension word to create account.
    - Need to support restore with extension word, before this commit should be accepted.

--- src/app/classes/wallet-creation.ts
+++ src/app/classes/wallet-creation.ts
@@ -1,6 +1,6 @@
 export class WalletCreation {
 
-  constructor(name: string, mnemonic: string, password: string, folderPath: string = null, passPhrase: string = null) {
+  constructor(name: string, mnemonic: string, password: string, passPhrase: string, folderPath: string = null) {
     this.name = name;
     this.mnemonic = mnemonic;
     this.password = password;
